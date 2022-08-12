1b7096d50ff37c94f839ad6366cc4e40b068492e
LuisGerar321 <Luis.Camara@theksquaregroup.com>
2022-08-12 13:30:25 -0500

    chore: added commands with choises and templates for test options

---
+++ generators/test/index.js
@@ -0,0 +1,140 @@
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const changeCase = require("change-case");
+
+const TestTypes = {
+  Controller: "controller",
+  Model: "model",
+  Service: "service",
+}
+module.exports = class extends Generator {
+  constructor(args, options) {
+    super(args, options);
+    this.silent = options.silent || false;
+    this.opts = options.opts || {};
+
+    this.argument("name", {
+      type: String,
+      required: false,
+      description: "Your testFile name",
+    });
+  }
+
+  prompting() {
+    if (!this.silent)
+      this.log(
+        "\nWelcome to the " + chalk.red("Flugzeug test") + " generator\n",
+      );
+
+    // Set default controller name to the name passed as arguments if opts.modelName is not present
+    if (this.opts.testName == null && this.options.name != null) {
+      this.opts.testName = changeCase.pascalCase(this.options.name);
+    }
+
+    const prompts = [
+      {
+        type: "list",
+        name: "testType",
+        message: " type:",
+        default: "controller",
+        choices: [
+          { name: "Service", value: TestTypes.Service},
+          { name: "Model", value: TestTypes.Model},
+          { name: "Controller", value: TestTypes.Controller},
+        ],
+      },
+    ];
+
+    const promptController = {
+      type: "input",
+      name: "controllerName",
+      message: "Controller name:",
+      default: this.opts.modelName == null ? "Thing" : this.opts.modelName,
+      filter: changeCase.pascalCase,
+    }
+
+    const promptModel = {
+      type: "input",
+      name: "modelName",
+      message: "Model:",
+      default: (props) => {
+        return this.opts.modelName == null
+          ? props.controllerName
+          : this.opts.modelName;
+      },
+      when: this.opts.modelName == null,
+    };
+
+    const promptService = {
+      type: "input",
+      name: "serviceName",
+      message: "Service:",
+      default: this.opts.modelName == null ? "Thing" : this.opts.modelName,
+      filter: changeCase.pascalCase,
+    }
+
+    return this.prompt(prompts).then(async (props) => {
+      this.props = props;
+      let promptsState;
+      switch (props.testType) {
+        case TestTypes.Service:
+          promptsState = await this.prompt([promptService]);
+          this.props = {...promptsState, ...this.props};   
+          break;
+        case TestTypes.Model:
+          promptsState = await this.prompt([promptModel]);
+          this.props = {...promptsState, ...this.props};          
+          break;
+        case TestTypes.Controller:
+          promptsState = await this.prompt([promptController, promptModel]);
+          this.props = {...promptsState, ...this.props};
+          break;
+        default:
+          break;
+      }
+
+      this.props = Object.keys(this.props).reduce((acc, key, _, object ) => {
+        acc[key] = object[key].toLowerCase();
+        return {...acc};
+      }, {})
+
+      Object.assign(this.opts, this.props);
+    });
+  }
+
+  writing() {
+    const fileName = this.props.name ??  ((this.props.controllerName ?? this.props.modelName) ??  this.props.serviceName);
+    switch (this.props.testType) {
+      case TestTypes.Service:
+        this.fs.copyTpl(
+          this.templatePath("../../test/templates/serviceTestTemplate.ts"),
+          this.destinationPath(
+            `app/test/basic/${fileName}.ts`,
+          ),
+          this.props,
+        );
+        break;
+      case TestTypes.Model:     
+      this.fs.copyTpl(
+        this.templatePath("../../test/templates/modelTestTemplate.ts"),
+        this.destinationPath(
+          `app/test/basic/${fileName}.ts`,
+        ),
+        this.props,
+      );
+      break;
+      case TestTypes.Controller:
+        this.fs.copyTpl(
+          this.templatePath("../../test/templates/controllerTestTemplate.ts"),
+          this.destinationPath(
+            `app/test/basic/${fileName}.ts`,
+          ),
+          this.props,
+        );
+        break;
+      default:
+        break;
+    }
+  }
+};
---
+++ generators/test/templates/modelTestTemplate.ts
@@ -0,0 +1,24 @@
+import { before, describe, it } from "mocha";
+import chai from "chai";
+import chaiHttp from "chai-http";
+import testDB from "@/test/util";
+import { config } from "@/config";
+import { app } from "@/server";
+import _ from "lodash";
+import chalk from "chalk";
+
+describe(chalk.magenta("Test <%- controllerName %> Endpoints"), function() {
+  let token = "";
+
+  before("Setup DB and get token", async function() {
+    this.timeout(50000);
+  });
+
+  describe(chalk.yellow(`message`), function () {
+    it(`should create an cute message`, async function() {
+      chai.expect({}).to.be.equal();
+      console.log("Hii bru <3")
+    });
+
+  })
+});
