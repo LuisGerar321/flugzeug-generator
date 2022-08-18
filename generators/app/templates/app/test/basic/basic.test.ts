ca2ccf5891ef8a3968f9896858e9b564e70fdf04
LuisGerar321 <Luis.Camara@theksquaregroup.com>
2022-08-18 14:50:10 -0500

    chore: added randomwords package, created generations functions to create objects in base on Seq schemas, added readmes to test folders, refactored basic.test.ts unit test

--- generators/app/templates/app/test/basic/basic.test.ts
+++ generators/app/templates/app/test/basic/basic.test.ts
@@ -2,19 +2,24 @@ require("dotenv").config();
 process.env.DB_NAME = "app-backend-test";
 
 import chai from "chai";
-import authService, { AuthCredentials, JWTPayload, Token } from "@/services/AuthService";
+import authService, {
+  AuthCredentials,
+  JWTPayload,
+  Token,
+} from "@/services/AuthService";
 import testDB from "@/test/util";
 import { log } from "@/libraries/Log";
 import { TokenExpiredError } from "jsonwebtoken";
 import { AuthType, User } from "@/models/User";
 import { Role } from "@/models/Role";
-import onboardingService from "@/services/OnboardingService"
+import onboardingService from "@/services/OnboardingService";
 import { UniqueConstraintError } from "sequelize/types";
 import _ from "lodash";
+import chalk from "chalk";
 
 let token;
 
-describe("Test basic app unit test", () => {
+describe(`${chalk.green("> Basic")} ${chalk.magenta("app")} ${chalk.green("unit test.")}`, () => {
   before(async function() {
     this.timeout(50000);
     await testDB.init();
@@ -22,7 +27,7 @@ describe("Test basic app unit test", () => {
       include: [{ model: Role, as: "roles" }],
     });
     const tokenCreation = await authService.createToken(user, "access");
-    token = tokenCreation.token
+    token = tokenCreation.token;
   });
 
   const jwtBody = {
@@ -35,18 +40,18 @@ describe("Test basic app unit test", () => {
     email: "admin@example.com",
     roles: [5],
   };
+
   const tokenExpired =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1wbG95ZWVJZCI6OSwic3ViIjoiYWNjZXNzIiwiYXVkIjoidXNlciIsImV4cCI6MC4zMTYsImlhdCI6MTY1NDYyMDA3NS4zMTYsImp0aSI6IjNiYjg3NGNkLTc4NjQtNDYwNy05YWY1LTcyY2VkZTY0NTU1ZCIsImVtYWlsIjoiYWRtaW5AZXhhbXBsZS5jb20iLCJyb2xlcyI6WzVdfQ.Zbt50VpQ7UIC1_3oC9uBVq3aDax227sDlOgtLEFflLc";
 
-  describe("#validateJWT()", () => {
-
+  describe(chalk.yellow("#validateJWT():"), () => {
     it("should be destructure jwt property a get token info", async () => {
       let jwt;
       try {
         jwt = await authService.validateJWT(token, "access");
       } catch (e) {
         jwt = null;
-      }
+      };
       chai.expect(jwt).to.have.all.keys(jwtBody);
     });
 
@@ -56,7 +61,7 @@ describe("Test basic app unit test", () => {
         jwt = await authService.validateJWT(token, "access");
       } catch (e) {
         jwt = null;
-      }
+      };
       chai.expect(jwt).to.be.an("object");
     });
 
@@ -66,12 +71,11 @@ describe("Test basic app unit test", () => {
       } catch (e) {
         chai.expect(e).to.be.instanceOf(TokenExpiredError);
         chai.expect(e.message).to.eql("jwt expired");
-      }
+      };
     });
   });
 
-  describe("#createToken()", () => {
-
+  describe(chalk.yellow("#createToken():"), () => {
     it("should create an access token in base of a user", async () => {
       const user = await User.findOne({
         include: [{ model: Role, as: "roles" }],
@@ -82,7 +86,7 @@ describe("Test basic app unit test", () => {
         tokenCreation = await authService.createToken(user, "access");
       } catch (e) {
         tokenCreation = null;
-      }
+      };
       chai.assert.isNotNull(tokenCreation);
     });
 
@@ -96,7 +100,7 @@ describe("Test basic app unit test", () => {
         tokenCreation = await authService.createToken(user, "reset");
       } catch (e) {
         tokenCreation = null;
-      }
+      };
       chai.assert.isNotNull(tokenCreation);
     });
 
@@ -104,8 +108,7 @@ describe("Test basic app unit test", () => {
       const user = await User.findOne({
         include: [{ model: Role, as: "roles" }],
       });
-      
-      
+
       let tokenCreation;
       const expectedToken: Token = {
         token:
@@ -138,11 +141,10 @@ describe("Test basic app unit test", () => {
       } catch (e) {
         chai.expect(e).to.be.instanceOf(ReferenceError);
         chai.expect(e.message).to.eql("roles was not included into user.");
-      }
+      };
     });
   });
-  describe("#getCredentials()", () =>{
-
+  describe(chalk.yellow("#getCredentials():"), () => {
     it("should not obtain the null credentials", async () => {
       const user = await User.findOne({
         include: [{ model: Role, as: "roles" }],
@@ -153,8 +155,7 @@ describe("Test basic app unit test", () => {
         userCredential = await authService.getCredentials(user);
       } catch (e) {
         userCredential = null;
-      }
-      
+      };
       chai.assert.isNotNull(userCredential);
     });
 
@@ -162,41 +163,39 @@ describe("Test basic app unit test", () => {
       const user = await User.findOne({
         include: [{ model: Role, as: "roles" }],
       });
-      
-      
+
       let userCredential;
       const expectedToken: AuthCredentials = {
-        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwic3ViIjoiYWNjZXNzIiwiYXVkIjoidXNlciIsImV4cCI6MTY1NzgxNjA3NS41NSwiaWF0IjoxNjU1MjI0MDc1LjU1LCJqdGkiOiIyODM2OTYwNy00MWNmLTQ2Y2MtOWFhYy04NWRjYzIwYmE5MTAiLCJlbWFpbCI6ImFkbWluQGV4YW1wbGUuY29tIiwicm9sZXMiOlsxXX0.arwSKCYXhBjKW7gcesh7Y05n2zwbDrMkCdtqTQL-pAo',
+        token:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwic3ViIjoiYWNjZXNzIiwiYXVkIjoidXNlciIsImV4cCI6MTY1NzgxNjA3NS41NSwiaWF0IjoxNjU1MjI0MDc1LjU1LCJqdGkiOiIyODM2OTYwNy00MWNmLTQ2Y2MtOWFhYy04NWRjYzIwYmE5MTAiLCJlbWFpbCI6ImFkbWluQGV4YW1wbGUuY29tIiwicm9sZXMiOlsxXX0.arwSKCYXhBjKW7gcesh7Y05n2zwbDrMkCdtqTQL-pAo",
         expires: 1657816075.55,
         refresh_token: {
-          token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwic3ViIjoicmVmcmVzaCIsImF1ZCI6InVzZXIiLCJleHAiOjE2NzEwMzg4NzUuNTUsImlhdCI6MTY1NTIyNDA3NS41NSwianRpIjoiYzU4Y2QyNDgtNjI5Yi00NmNmLWI5YjktZDRmOTkyOTU3OWYwIiwiZW1haWwiOiJhZG1pbkBleGFtcGxlLmNvbSIsInJvbGVzIjpbMV19.pdkHBOHJtg_TSbHATFlFJiej18mCUH12h1O3_Nu-UgE',
+          token:
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwic3ViIjoicmVmcmVzaCIsImF1ZCI6InVzZXIiLCJleHAiOjE2NzEwMzg4NzUuNTUsImlhdCI6MTY1NTIyNDA3NS41NSwianRpIjoiYzU4Y2QyNDgtNjI5Yi00NmNmLWI5YjktZDRmOTkyOTU3OWYwIiwiZW1haWwiOiJhZG1pbkBleGFtcGxlLmNvbSIsInJvbGVzIjpbMV19.pdkHBOHJtg_TSbHATFlFJiej18mCUH12h1O3_Nu-UgE",
           expires: 1671038875.55,
-          expires_in: 15814800
+          expires_in: 15814800,
         },
-        user: { id: 1, name: 'Admin', email: 'admin@example.com' },
+        user: { id: 1, name: "Admin", email: "admin@example.com" },
         profile: undefined,
         roles: [],
-      }
-  
+      };
+
       try {
         userCredential = await authService.getCredentials(user);
       } catch (e) {
         userCredential = null;
-      }
+      };
       chai.expect(userCredential).to.be.an("object");
       chai.expect(userCredential).to.have.keys(Object.keys(expectedToken));
     });
+  });
 
-  })
-
-  describe("#createUser", () => {
-
+  describe(chalk.yellow("#createUser():"), () => {
     it("should not obtain the null credentials", async () => {
       let newUser: User;
-
-      let userCredential;
       try {
-        newUser = await onboardingService.createUser( "Jorge",
+        newUser = await onboardingService.createUser(
+          "Jorge",
           "jorge.test@theksquaregroup.com",
           "testpass",
           AuthType.Email,
@@ -205,8 +204,7 @@ describe("Test basic app unit test", () => {
         );
       } catch (e) {
         newUser = null;
-      }
-      
+      };
       chai.assert.isNotNull(newUser);
     });
 
@@ -216,33 +214,18 @@ describe("Test basic app unit test", () => {
         isActive: false,
         isEmailConfirmed: false,
         id: 2,
-        name: 'Luis',
-        email: 'luis.test@theksquaregroup.com',
-        password: '$2b$10$QqbA.msasnjpF4EYDLaZyeGRRjp7iWwRXGu.X0sjtpXuWDAkGHd.6',
+        name: "Luis",
+        email: "luis.test@theksquaregroup.com",
+        password:
+          "$2b$10$QqbA.msasnjpF4EYDLaZyeGRRjp7iWwRXGu.X0sjtpXuWDAkGHd.6",
         authType: AuthType.Email,
-        firstName: 'Camara',
-        lastName: 'Salinas',
+        firstName: "Camara",
+        lastName: "Salinas",
       };
-      try {
-        newUser = await onboardingService.createUser( "Luis",
-          "luis.test@theksquaregroup.com",
-          "testpass",
-          AuthType.Email,
-          "Camara",
-          "Salinas",
-        );
-      } catch (e) {        
-        newUser = null;
-      }
-      const userOutput = _.pick(newUser, Object.keys(userCreated))
-      chai.expect(newUser).to.be.an("object");
-      chai.expect(userOutput).to.have.keys(Object.keys(userCreated));
-    });
 
-    it("should throw an Error because its trying to create a new user with an alrready existent user email in db.", async () => {
-      let newUser: User;
       try {
-        newUser = await onboardingService.createUser( "Luis",
+        newUser = await onboardingService.createUser(
+          "Luis",
           "luis.test@theksquaregroup.com",
           "testpass",
           AuthType.Email,
@@ -250,10 +233,27 @@ describe("Test basic app unit test", () => {
           "Salinas",
         );
       } catch (e) {
-        console.log("Error name: ", e.name);
-        chai.expect(e).to.be.instanceOf(Object);
-      }
-      
+        newUser = null;
+      };
+      const userOutput = _.pick(newUser, Object.keys(userCreated));
+      chai.expect(newUser).to.be.an("object");
+      chai.expect(userOutput).to.have.keys(Object.keys(userCreated));
     });
-  })
+
+    it("should throw an Error because its trying to create a new user with an alrready existent user email in db.", async () => {
+      let newUser: User;
+      try {
+        newUser = await onboardingService.createUser(
+          "Luis",
+          "luis.test@theksquaregroup.com",
+          "testpass",
+          AuthType.Email,
+          "Camara",
+          "Salinas",
+        );
+      } catch (e) {
+        chai.expect(e).to.be.instanceOf(Object);
+      };
+    });
+  });
 });
--- generators/app/templates/app/test/util.ts
+++ generators/app/templates/app/test/util.ts
@@ -2,9 +2,178 @@ import { Client } from "pg";
 import { config } from "@/config";
 import { db, setupDBClearData } from "@/db";
 import { seed } from "@/seedData";
+import _ from "lodash";
+import { getSequelizeTypeByDesignType } from "sequelize-typescript";
+import { DataTypes, INTEGER } from "sequelize";
+import randomWords from "random-words";
+import { Model } from "sequelize";
+
+/* handleTestWhatEver are functions that execute the CRUD in Tables
+ *  The reason of using this functions is to take care into the code scalability
+ *  In case of a modification on Sequelize CRUD methods, just modify that.
+*/
+
+/**
+ * Builds a new model instance and calls save on it.
+ * @param model model to call the metod
+ * @param entry values
+ * @returns
+ */
+export async function handleTestCreate(model: any, entry: Object): Promise<any> {
+  try {
+    const seqObj = await model.create(entry);
+    return seqObj;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+/**
+ * Search for a single instance.
+ * @param model
+ * @param id 
+ */
+export async function handleTestRead(model: any, id: number) {
+  try {
+    const seqObj = await model.findByPk(id);
+    return seqObj;
+  } catch (error) {
+    throw error;
+  }
+}
+
+/**
+ * Update a model instance.
+ * @param seqObjToUpdate Sequelize model to update.
+ * @param entry values to update.
+ */
+export async function handleTestUpdate(seqObjToUpdate: any, entry: Object) {
+  try {
+    const seqObj = await seqObjToUpdate.update(entry);
+    return seqObj;
+  } catch (error) {
+    throw error;
+  }
+}
+
+/**
+ * Delete a model instance
+ * @param seqObjToDestroy Sequelize model to destroy.
+ */
+export function handleTestRemove(seqObjToDestroy) {
+  try {
+    return seqObjToDestroy.destroy();
+  } catch (error) {
+    throw error;
+  }
+}
+
 export function wait(ms) {
   return new Promise((r, _) => setTimeout(r, ms));
 }
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+/* Provides a JS data type in base of a Sequelize.DataTypes from an Schema */
+//TODO: Add more logic for BIG INT, AND LENGT FROM TYPE
+export function getTypeFromSeqDataTypes(dataType: Object): String {
+
+  const dataTypeValue = String(dataType);
+
+  if (dataTypeValue === "" || dataTypeValue === "null") return "null";
+
+  //Common words types into Sequalize.DataTypes
+  const stringType = ["STRING", "TEXT", "CHAR", "TSVECTOR"];
+  const intType = ["INT", "INTEGER"];
+  const decimalType = ["REAL", "FLOAT", "DOUBLE", "DECIMAL"];
+  const booleanType = ["BOOLEAN"];
+
+  //Check if any key match into dataType string
+  const isString = stringType.some(element => dataTypeValue.includes(element));
+  const isInt = intType.some(element => dataTypeValue.includes(element));
+  const isDecimal = decimalType.some(element => dataTypeValue.includes(element));
+  const isBoolean = booleanType.some(element => dataTypeValue.includes(element));
+  
+  if (isString) return "string";
+  
+  if (isBoolean) return "boolean";
+
+  if (isDecimal || isInt) return "number";
+
+  return "undefined";
+}
+export const timeStampKeys = ["updatedAt", "createdAt"];
+/**
+ * Get the current schema from a Sequelize Model
+ * @param Model  The Sequelize Model to read, e.g. User, Thing, etc.
+ */
+export function getSchema(Model: any) {
+  return _.omit(Model.prototype["rawAttributes"], [...timeStampKeys, "id"]); 
+}
+
+/**Generate randon object entries from schema provided.
+ It is useful when the user wants to generate a object with the attributes that are into the sequelize schema provided.
+  @param seqSchema A Sequelize schama.
+  @param skipAllowNull A boolean entry that determine if user wants to create attributes that allows null into the Schema provited. Otherwise, it will be ignored. So, if the flag is setted as true, It will be ignore that props and it wont be created.
+*/
+
+export function generateRndEntriesFromSchema(seqSchema: Object, skipAllowNull: Boolean = true): Object {
+  const obj =  Object.keys(seqSchema).reduce((acc, key) => {
+    //If schema allows null value, lets set the default value;
+    if (seqSchema[key].allowNull === true && skipAllowNull === true) return acc;
+
+    const dataType = seqSchema[key]["type"];
+    
+    switch ( getTypeFromSeqDataTypes(dataType) ) {
+      case "number":
+        acc[key] = getRndInteger(1, 100) ;
+        break;
+      case "string":
+        acc[key] = randomWords(5)[0];
+        break;
+      case "boolean":
+        acc[key] = !!getRndInteger(0,1);
+        break;
+    };
+
+    return acc;
+  }, {});
+  return obj;
+}
+
+/**Generate randon object entries from schema provided.
+ It is useful when the user wants to generate a object with the attributes that are into the sequelize schema provided.
+  @param seqSchema A Sequelize schama.
+  @param skipAllowNull A boolean entry that determine if user wants to create attributes that allows null into the Schema provited. Otherwise, it will be ignored. So, if the flag is setted as true, It will be ignore that props and it wont be created.
+*/
+export function generateEmptyEntriesFromSchema(seqSchema: Object, skipAllowNull: Boolean = true): Object {
+  const obj =  Object.keys(seqSchema).reduce((acc, key) => {
+    //If schema allows null value, lets set the default value;
+    if (seqSchema[key].allowNull === true || skipAllowNull === true) return acc;
+
+    const dataType = seqSchema[key]["type"];
+    
+    switch ( getTypeFromSeqDataTypes(dataType) ) {
+      case "number":
+        acc[key] = null;
+        break;
+      case "string":
+        acc[key] = "";
+        break;
+      case "boolean":
+        acc[key] = null;
+        break;
+    };
+
+    return acc;
+  }, {});
+  return obj;
+}
+
+
 export async function createDB() {
   const connection = new Client({
     host: config.db.host,
@@ -64,25 +233,24 @@ export class TestDB {
 
   public static getInstance(): TestDB {
     if (!TestDB.instance) {
-        TestDB.instance = new TestDB();
+      TestDB.instance = new TestDB();
     }
 
     return TestDB.instance;
   }
 
   //Executed once when code intence it.
-  async init(){
-      
-      if (!!this.isAlreadyInit) return this.db;
-    
-      await createDB();
-      await addExtensions();
-      this.db = await setupDBClearData();
-      await seed();
-      this.isAlreadyInit = true;
-      return this.db;
+  async init() {
+    if (!!this.isAlreadyInit) return this.db;
+
+    await createDB();
+    await addExtensions();
+    this.db = await setupDBClearData();
+    await seed();
+    this.isAlreadyInit = true;
+    return this.db;
   }
 }
 
-const testDB =  TestDB.getInstance(); //Singleton creation.
+const testDB = TestDB.getInstance(); //Singleton creation.
 export default testDB;
